044b690a00c11d0ab3726c1a5e427fdbec8f5fc7
surunzi <670785827@qq.com>
2015-02-21 22:33:27 +0800

    Add: Module asset and save

---
+++ engine/core/asset.js
@@ -0,0 +1,9 @@
+// Module asset
+
+webvn.add('asset', ['util'], function (s, util) {
+
+var asset = {};
+
+return asset;
+
+});
---
+++ engine/core/save.js
@@ -0,0 +1,40 @@
+/* Save and load module
+ * Everything that is about game saving and loading is defined here
+ */
+
+webvn.add('save', ['util', 'class'], function (s, util, kclass) {
+
+var save = {},
+    cache = {};
+
+var Save = kclass.create({
+    constructor: function Save() {
+
+
+
+    }
+});
+
+save.create = function (name) {
+
+
+
+};
+
+save.create('save-auto');
+
+save.load = function () {
+
+
+
+};
+
+save.save = function () {
+
+
+
+};
+
+return save;
+
+});
