93a0a26930c6015a6a5230ba564a780b777b4813
surunzi <670785827@qq.com>
2015-02-27 21:48:57 +0800

    Add: Voice and se command

---
+++ engine/cmd/audio.js
@@ -0,0 +1,83 @@
+// Command bgm
+
+webvn.use(['script', 'audio'], function (s, script, audio) {
+
+// Background music
+var bgm = audio.create('bgm');
+
+script.addCommand('bgm', {
+    loop: {
+        type: 'Boolean',
+        shortHand: 'l'
+    },
+    src: {
+        type: 'String',
+        shortHand: 's'
+    }
+}, function (options, value) {
+
+    if (options.src) {
+        bgm.load(options.src);
+    }
+
+    if (options.loop === true) {
+        bgm.setLoop(true);
+    } else if (options.loop === false) {
+        bgm.setLoop(false);
+    }
+
+});
+
+// Sound Effect
+var se = audio.create('se');
+
+script.addCommand('se', {
+    loop: {
+        type: 'Boolean',
+        shortHand: 'l'
+    },
+    src: {
+        type: 'String',
+        shortHand: 's'
+    }
+}, function (options, value) {
+
+    if (options.src) {
+        se.load(options.src);
+    }
+
+    if (options.loop === true) {
+        se.setLoop(true);
+    } else if (options.loop === false) {
+        se.setLoop(false);
+    }
+
+});
+
+// Voice
+var voice = audio.create('voice');
+
+script.addCommand('voice', {
+    loop: {
+        type: 'Boolean',
+        shortHand: 'l'
+    },
+    src: {
+        type: 'String',
+        shortHand: 's'
+    }
+}, function (options, value) {
+
+    if (options.src) {
+        voice.load(options.src);
+    }
+
+    if (options.loop === true) {
+        voice.setLoop(true);
+    } else if (options.loop === false) {
+        voice.setLoop(false);
+    }
+
+});
+
+});
